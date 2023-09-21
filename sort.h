7d8a4b91ee10d439987db997f30c3b1dda5bd39f
Ade-nike17 <perfectnyke@gmail.com>
2023-09-21 10:06:32 -0700

    modified header file

--- sort.h
+++ sort.h
@@ -24,8 +24,16 @@ void bubble_sort(int *array, size_t size);
 listint_t *create_listint(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
+
 /* Task functions */
 void bubble_sort(int *, size_t);
 void selection_sort(int *, size_t);
+
+/* Quick sort functions */
+void swap(int *a, int *b);
+int lomuto_partition(int *array, int high, int low, size_t size);
+void _quicksort(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
+
 
 #endif /* SORT_H */
